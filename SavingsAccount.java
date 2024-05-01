afbd0e8548c61bdc8bb309f3b3625b968bc1aa01
WTC <zngeyane023@student.wethinkcode.co.za>
2024-05-01 16:26:52 +0200

    SavingsAccount class

---
+++ SavingsAccount.java
@@ -0,0 +1,12 @@
+public class SavingsAccount extends BankAccount{
+    private final double interestRate;
+
+    public SavingsAccount(String accountNumber, double initialBalance, double interestRate) {
+        super(accountNumber, initialBalance);
+        this.interestRate = interestRate;
+    }
+}
+
+
+
+
