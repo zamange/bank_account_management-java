7a41f4906e9907bbde92d1839a4aa9be814c0903
WTC <zngeyane023@student.wethinkcode.co.za>
2024-05-01 13:52:29 +0200

    constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,11 @@ public class BankAccount {
     private String accountNumber;
     private double balance;
 
+
+    //constructor
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
 }
